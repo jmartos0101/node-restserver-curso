f2275eb2982722ce5d1791a03e7012e56ae89265
Jose <jmartos0101@gmail.com>
2018-12-21 14:06:01 +0100

    asdsad

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    urlDB = process.env.MONGO_UIR 
+    urlDB = process.env.MONGO_URI 
 }
 
 process.env.URLDB = urlDB;
