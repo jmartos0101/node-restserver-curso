89381326b15adbf556466ce7faeef2b4c2e19115
Jose <jmartos0101@gmail.com>
2018-12-21 13:46:59 +0100

    Segundo commit

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,18 @@
 
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BASE DE DATOS
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe'
+}else{
+    urlDB = 'mongodb://cafe-user:sapphire1@ds235053.mlab.com:35053/cafe'
+}
+
+process.env.URLDB = urlDB;
---
+++ server/models/usuario.js
@@ -0,0 +1,62 @@
+
+const moongose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un role valido'
+};
+
+let Schema = moongose.Schema;
+
+
+let usuarioSchema = new Schema({
+    nombre:{
+        type: String,
+        required: [true, 'EL nombre es necesario']
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password:{
+        type: String,
+        required: [true, 'La pass es obligatoria']
+    },
+    img:{ 
+        type: String
+
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado:{
+        type: Boolean,
+        default: true   
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+usuarioSchema.methods.toJSON = function() {
+    
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico'})
+
+
+module.exports = moongose.model('Usuario', usuarioSchema);
